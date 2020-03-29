ea13db3ca7abfb712dd82dc0665e642db3547b3f
Alexander Pirnarev <sashopirnarev@gmail.com>
2020-03-29 13:04:56 +0300

    add img for keyboard,lence and mic

--- src/App.jsx
+++ src/App.jsx
@@ -28,26 +28,46 @@ function App() {
           />
         </div>
       </div>
+      <div id="main">
+        <img
+          id="google-logo"
+          src="https://www.google.bg/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
+          alt="google logo"
+        />
+        <div>
+          <div className="searchBox">
+            <img
+              className="searchBox-item"
+              src="https://cdn4.iconfinder.com/data/icons/user-interface-175/32/UIF-121-512.png"
+              alt="lens"
+              height="20px"
+              width="20px"
+            />
+            <input type="text" className="searchBox-item" />
+            <img
+              className="searchBox-item"
+              src="https://lh3.googleusercontent.com/proxy/01BzJclrspOQC-RWZ5ub4O-0-S9sjPGOZUxkJEHkeAsHgrq_mZkRFMXzUdVr5Bc6WoU4q0CoEANXZHDKXUuCO9OVYxBXBSxW6IFfvBBBYl8KfBU-ZgZ9x8c"
+              alt="keyboard"
+              height="30px"
+              width="30px"
+            />
+            <img
+              className="searchBox-item"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOdX8ck7y7RlW8jUex4EiAMF-aHNk5tJk-4U_w02Z6GFCEkEM&s"
+              alt="mic"
+              height="40px"
+              width="30px"
+            />
+          </div>
 
-      <img
-        id="google-logo"
-        src="https://www.google.bg/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
-        alt="google logo"
-      />
-      <div>
-        <div className="searchBox">
-          <div className="searchBox-item">Lens</div>
-          <input type="text" className="searchBox-item" />
-          <div className="searchBox-item">Microphone</div>
-        </div>
-
-        <div className="searchButtons">
-          <button type="submit" className="searchBox-button">
-            Google Search
-          </button>
-          <button type="submit" className="searchBox-button">
-            I'm feeling lucky KEKW
-          </button>
+          <div className="searchButtons">
+            <button type="submit" className="searchBox-button">
+              Google Search
+            </button>
+            <button type="submit" className="searchBox-button">
+              I'm feeling lucky KEKW
+            </button>
+          </div>
         </div>
       </div>
       <footer id="footer">
