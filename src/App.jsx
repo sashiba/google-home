f45247052dfa71a5df50427c5254c66651094ce3
Alexander Pirnarev <sashopirnarev@gmail.com>
2020-03-29 11:56:34 +0300

    apply styles to footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,71 @@ import './App.css';
 
 function App() {
   return (
-    // navbar(gmil, iimages, apps, profile)
-
-    // search(lupa, textfiled, keyboart, mic)
-
     <div className="App">
-      <div>
-        <div>
+      <div className="navbar">
+        <div className="navbar-child">
           <a href="#">Gmail</a>
         </div>
-        <div>
+        <div className="navbar-child">
           <a href="#">Images</a>
         </div>
-        <div> Applications</div>
-        <div>Profile</div>
+        <div className="navbar-child">
+          <img
+            src="https://cdn3.iconfinder.com/data/icons/navigation-and-settings/24/Material_icons-01-11-512.png"
+            alt="Applications icon"
+            width="30px"
+            height="30px"
+          />
+        </div>
+        <div className="navbar-child">
+          <img
+            src="https://cdn1.iconfinder.com/data/icons/mix-color-4/502/Untitled-1-512.png"
+            alt="profile icon"
+            width="30px"
+            height="30px"
+          />
+        </div>
       </div>
 
       <img
+        id="google-logo"
         src="https://www.google.bg/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
         alt="google logo"
       />
       <div>
-        <div>Lens</div>
-        <input type="text" />
-        <div>Microphone</div>
-      </div>
+        <div className="searchBox">
+          <div className="searchBox-item">Lens</div>
+          <input type="text" className="searchBox-item" />
+          <div className="searchBox-item">Microphone</div>
+        </div>
 
-      <button type="submit">Google Search</button>
-      <button type="submit">I'm feeling lucky KEKW</button>
-      <footer>
-        <div>Advertising Business About How Search works Privacy Terms Settings</div>
+        <div className="searchButtons">
+          <button type="submit" className="searchBox-button">
+            Google Search
+          </button>
+          <button type="submit" className="searchBox-button">
+            I'm feeling lucky KEKW
+          </button>
+        </div>
+      </div>
+      <footer id="footer">
+        <div>
+          <div className="footer-container-left">
+            <span className="footer-child">
+              <a href="#">Advertising</a>
+              <a href="#">Business</a>
+              <a href="#">About</a>
+              <a href="#">How Search works</a>
+            </span>
+          </div>
+          <div className="footer-container-right">
+            <span className="footer-child">
+              <a href="#">Privacy</a>
+              <a href="#">Terms</a>
+              <a href="#">Settings</a>
+            </span>
+          </div>
+        </div>
       </footer>
     </div>
   );
